89500ce8b25e501ecf9d9f708b8a3c9a2e3239ed
S Shahrukh <37923161+shahrukhsyed@users.noreply.github.com>
2019-07-06 13:18:11 +0530

    Add files via upload

---
+++ Lab 2-2/src/app/app.module.ts
@@ -0,0 +1,19 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { SortEmployeeComponent }  from './app.sortEmployee';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+    imports: [
+        BrowserModule,FormsModule
+        
+    ],
+    declarations: [
+        AppComponent,SortEmployeeComponent
+		],
+    providers: [ ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }

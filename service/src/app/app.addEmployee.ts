483bb583f2a0dd3d3f69ff654d4e091ef0d77180
S Shahrukh <37923161+shahrukhsyed@users.noreply.github.com>
2019-07-09 14:25:21 +0530

    Add files via upload

---
+++ service/src/app/app.addEmployee.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+    selector: 'add-comp',
+    templateUrl: 'addemployee.html'
+})
+
+export class AddEmployeeComponent {
+    
+  empAll:any[]=[];
+
+}

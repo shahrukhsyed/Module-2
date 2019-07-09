a8dc5fa01c12e920469040ab3b8932c3e7bc0bc0
S Shahrukh <37923161+shahrukhsyed@users.noreply.github.com>
2019-07-09 14:23:46 +0530

    Add files via upload

---
+++ ass4.1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import {AddEmployeeComponent } from './app.addEmployee';
+import {ShowEmployeeComponent } from './app.showEmployee';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+    imports: [
+        BrowserModule,HttpClientModule
+        
+    ],
+    declarations: [
+        AppComponent,AddEmployeeComponent,ShowEmployeeComponent
+		],
+    providers: [ ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
